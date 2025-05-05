c797b2ae6def7700cf2402a7e41c28a31d4da965
wioo-wint <wintshweyioo77@gmail.com>
2025-05-05 17:08:33 +0700

    Add files via upload

--- cell04/ex04/round_up.py
+++ cell04/ex04/round_up.py
@@ -1,6 +1,3 @@
+import math
 input_num = float(input("Give me a number: "))
-result = round(input_num)
-if result > input_num or result == input_num:
-    print(result)
-else:
-    print(result+1)
+print(math.ceil(input_num))
